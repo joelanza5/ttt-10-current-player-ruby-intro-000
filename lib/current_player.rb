7d83ab68b787b581779b280cf756e1d700371020
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-26 22:14:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board)
   counter = 0
-  board.each {|position|
+  board.each do |position|
     if position == "X" || position == "O"
       counter += 1
     end
-  }
+  end
   counter
 end
